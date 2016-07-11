57dd2d0afa773b791b50d42309d9026a0ae2d0ba
zanpakutou <lienlienlien369@gmail.com>
2016-07-11 21:54:49 +0700

    Update SNum.cpp

--- SNum.cpp
+++ SNum.cpp
@@ -2,45 +2,50 @@
 
 using namespace std;
 int bt[100];
+int res[100];
 int dp[70][20][2][2];
-int n,k,d;
-bool get(int n,int i)
-{
-       return (n>>i)&1;
-}
+long long n,k,d;
 void  pt(int n)
 {
        memset(bt,0,sizeof(bt));
-       for(int i=0;i<=64;i++) bt[64-i]=get(n,i);
+       for(int i=0;i<=61;i++){
+        bt[61-i]= n%2;
+        n/=2;
+       }
+}
+void bug()
+{
+    for(int i=55;i<=61;i++) cout<<res[i];
+    cout<<'\n';
 }
 int calc(int i,int sD,bool mean,bool les)
 {
        int &t= dp[i][sD][mean][les];
        if (t>-1) return t;
-       if(i>64) { t=(sD==k)?1:0; return t;};
+       if(i>61) {if (sD==k) t=1; else t=0;  return t;};
        t=0;
-       t+=calc(i+1,sD+mean?1:0,mean,les||bt[i]); //cout<<0;
-       if (bt[i]==1||les) {t+=calc(i+1,sD,1,les);};
+       int c=0;
+       if (mean) c=1;
+       res[i]=0;
+       t+=calc(i+1,sD+ c,mean,les||bt[i]);
+       if (bt[i]==1||les) {res[i]=1; t+=calc(i+1,sD,1,les);};
        return t;
 }
+
 int CH(int a)
 {
        memset(dp,-1,sizeof(dp));
-
        n=a;
        pt(n);
-       //d=log2(n);
-       //for(int i=0;i<=64;i++) cout<<bt[i];
        int x= calc(0,0,0,0);
        return x;
 }
+
 int main()
 {
 
-       int a,b,k;
-       cin>>b>>k;
-       cout<<CH(b);
-       //pt(b);
-
+       long long  a,b;
+       cin>>a>>b>>k;
+       cout<<CH(b)-CH(a-1);
        return 0;
 }
