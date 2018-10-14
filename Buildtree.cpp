894845a5d5cc7aeccbe11c7ea9279f275ab087c8
zanpakutou <lienlienlien369@gmail.com>
2018-10-14 21:40:59 +0700

    Add files via upload

---
+++ Buildtree.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define N   1000
+using namespace std;
+struct edge
+{
+    int x,y,c;
+};
+edge e[N];
+int n, m, vt[N];
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin>>n>>m;
+    for(int i=1;i<=m;i++) cin>>e[.x>>e.y>>e.c;
+    for(int i=1;i<m;i++) for(int j=i+1;j<=m;j++) if ()
+}
